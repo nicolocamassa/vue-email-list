855a8547bccee7ece121c12002ea9e1e63541bb2
nicolocamassa <camassanicolo03@gmail.com>
2023-12-20 15:58:55 +0100

    [FIX] array length fixed to 10

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ createApp({
     };
   },
   created() {
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i < 10; i++) {
       axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then((response) => {
